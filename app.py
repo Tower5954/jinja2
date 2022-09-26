0960d5ac3d8d60777eed69ba8c6dab945e0d3ab4
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2022-09-26 20:14:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ todos = [
 ]
 
 
+def square(value):
+    return (value ** 0.5).is_integer()
+
+
+app.jinja_env.tests["square"] = square
+
+
 @app.route("/")
 def todo():
     return render_template("home.html", todos=todos)
@@ -22,3 +29,8 @@ def todo_item(todo: str):
             return render_template("todo.html", text=text, completed=completed, title=title)
     else:
         return render_template("not-found.html", text=todo, title="Not found")
+
+
+@app.route("/fizzbuzz")
+def fizz():
+    return render_template("fizzbuzz.html")
